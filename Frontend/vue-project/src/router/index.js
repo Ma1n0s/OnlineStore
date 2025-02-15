46263ce6b436cb4db676c9ae5467be56c331427d
Ma1n0s <kirill000e@gmail.com>
2025-02-15 13:38:22 +0500

    Добавление url ссылки

--- Frontend/vue-project/src/router/index.js
+++ Frontend/vue-project/src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Main from '../views/Main.vue';
 import About from '../views/About.vue';
+import StroitelnyjInstrument from '@/views/category/stroitelnyj-instrument.vue';
+import Shurupoverty from '@/views/category/shurupoverty.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +16,16 @@ const router = createRouter({
       path: '/about',
       name: 'About',
       component: About,
+    },
+    {
+      path: '/category/troitelnyj-instrument',
+      name: 'Stroitelnyj-instrument',
+      component: StroitelnyjInstrument,
+    },
+    {
+      path: '/category/shurupoverty',
+      name: 'Shurupoverty',
+      component: Shurupoverty,
     },
 
   ]
