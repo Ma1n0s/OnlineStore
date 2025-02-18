f9b6766386666c63bd539f91576e68d0d599d3ab
Ma1n0s <kirill000e@gmail.com>
2025-02-18 12:48:33 +0500

    слайдер

--- Frontend/vue-project/src/views/Main2.vue
+++ Frontend/vue-project/src/views/Main2.vue
@@ -121,50 +121,85 @@ import { ref, computed  }  from "vue";
 
     <h1 class="font-bold text-2xl mb-4">ВсеИнструменты.ру – ваш надежный поставщик</h1>
     <div class="flex space-x-8">
-      <div class="shadow-lg h-52 w-52 rounded-lg flex flex-col">
-          <div class="flex items-start">
-              <img src="/public/box.svg" alt="box" class="w-10 h-10  m-4" />
-          </div>
-        <div class="flex-grow flex items-center justify-center">
-              <div class="text-start ml-4">
-                  1 500 000 товаров в наличии
-              </div>
-          </div>
-          <div class="text-gray-600 hover:text-rose-700 text-left ml-4 mb-2">
-              Подробнее
+    <div class="shadow-lg h-52 w-52 rounded-lg flex flex-col">
+        <div class="flex items-start">
+            <img src="/public/box.svg" alt="box" class="w-10 h-10 m-4" />
         </div>
-      </div>
-      <div class="shadow-lg h-52 w-52 rounded-lg flex flex-col">
-          <div class="flex items-start">
-              <img src="/public/box.svg" alt="box" class="w-10 h-10  m-4" />
-          </div>
         <div class="flex-grow flex items-center justify-center">
-              <div class="text-start ml-4">
-                  1 500 000 товаров в наличии
-              </div>
-          </div>
-          <div class="text-gray-600 hover:text-rose-700 text-left ml-4 mb-2">
-              Подробнее
+            <div class="text-start ml-4">
+                1 500 000 товаров в наличии
+            </div>
         </div>
-      </div>
-      <div class="shadow-lg h-52 w-52 rounded-lg flex flex-col">
-          <div class="flex items-start">
-              <img src="/public/box.svg" alt="box" class="w-10 h-10  m-4" />
-          </div>
-        <div class="flex-grow flex items-center justify-center">
-              <div class="text-start ml-4">
-                  1 500 000 товаров в наличии
-              </div>
-          </div>
-          <div class="text-gray-600 hover:text-rose-700 text-left ml-4 mb-2">
-              Подробнее
-        </div>
-      </div>
-        <div>
-          <img src="/public/8c696bc17de6aed2f1015d3612934a6b.jpeg" alt="logo" class="rounded-xl"/>
+        <div class="text-gray-600 hover:text-rose-700 text-left ml-4 mb-2">
+            Подробнее
         </div>
     </div>
+    <div class="shadow-lg h-52 w-52 rounded-lg flex flex-col">
+        <div class="flex items-start">
+            <img src="/public/time.svg" alt="time" class="w-10 h-10 m-4" />
+        </div>
+        <div class="flex-grow flex items-center justify-center">
+            <div class="text-start ml-4">
+               99% заказов доставляем в срок
+            </div>
+        </div>
+        <div class="text-gray-600 hover:text-rose-700 text-left ml-4 mb-2">
+            Подробнее
+        </div>
+    </div>
+    <div class="shadow-lg h-52 w-52 rounded-lg flex flex-col">
+        <div class="flex items-start">
+            <img src="/public/locationMain.svg" alt="locationMain" class="w-10 h-10 m-4" />
+        </div>
+        <div class="flex-grow flex items-center justify-center">
+            <div class="text-start ml-4">
+                1050+ магазинов по всей Росии
+            </div>
+        </div>
+        <div class="text-gray-600 hover:text-rose-700 text-left ml-4 mb-2">
+            Подробнее
+        </div>
+    </div>
+    <div class="shadow-lg h-52  rounded-lg overflow-hidden">
+        <img src="/public/8c696bc17de6aed2f1015d3612934a6b.jpeg" alt="logo" class="w-full h-full object-cover" />
+    </div>
 </div>
+
+    <div class="flex items-center">
+      <h1 class="font-bold text-2xl mb-2 mr-4">Только оригинальный товары</h1>
+      <RouterLink to="" class="text-sm hover:text-rose-700">Смотреть все</RouterLink>
+    </div>
+    <div class="relative w-full max-w-lg mx-auto">
+    <div class="overflow-hidden rounded-lg shadow-lg">
+      <div
+        class="flex transition-transform duration-300"
+        :style="{ transform: `translateX(-${currentIndex2 * (100 / 3)}%)` }"
+      >
+        <div
+          v-for="(slideGroup, index) in groupedSlides"
+          :key="index"
+          class="flex min-w-full"
+        >
+          <div
+            v-for="(slide, slideIndex) in slideGroup"
+            :key="slideIndex"
+            class="w-1/3 p-2"
+          >
+            <img :src="slide.image" :alt="slide.title" class="w-full h-auto rounded-lg" />
+            <h2 class="text-center text-xl font-bold">{{ slides2.title }}</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    <button @click="prevSlide2"  class="absolute top-1/2 left-4 transform -translate-y-1/2 text-white bg-black rounded-full w-8 h-8 flex items-center justify-center p-2">
+      &#10094;
+    </button>
+    <button @click="nextSlide2" class="absolute top-1/2 right-4 transform -translate-y-1/2 text-white bg-black rounded-full w-8 h-8 flex items-center justify-center p-2">
+      &#10095;
+    </button>
+  </div>
+  </div>
   </template>
   
   
