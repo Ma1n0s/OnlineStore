df22c377244004297a3d97a8159eef7aac41d0df
Ma1n0s <kirill000e@gmail.com>
2025-02-19 13:31:34 +0500

    Удалниен yandex-map

--- Frontend/vue-project/src/main.js
+++ Frontend/vue-project/src/main.js
@@ -1,16 +1,11 @@
 import './assets/main.css';
 
 import { createApp } from 'vue';
-import { createYmaps } from 'vue-yandex-maps';
 import App from './App.vue';
 import router from './router';
 import './assets/tailwind.css';
 
 const app = createApp(App);
-
-app.use(createYmaps({
-    apikey: '2d681ad7-e82e-47e4-97d2-8198a44c425b',
-}));
 
 app.use(router);
 app.mount('#app');
--- Frontend/vue-project/src/views/Courier-delivery.vue
+++ Frontend/vue-project/src/views/Courier-delivery.vue
@@ -1,27 +1,105 @@
+<script setup lang="ts">
+  import { ref } from 'vue';
+  import { shallowRef } from 'vue';
+  import Header from './Header.vue';
+  import Footer from './Footer.vue';
+
+  const isMkadInsideVisible = ref(false);
+  const isMkadOutsideVisible = ref(false);
+  const toggleMkadInside = () => {
+    isMkadInsideVisible.value = !isMkadInsideVisible.value;
+  };
+  const toggleMkadOutside = () => {
+    isMkadOutsideVisible.value = !isMkadOutsideVisible.value;
+  };
+</script>
 <template>
-    <yandex-map
-      v-model="map"
-      :settings="mapState"
-      width="100%"
-      height="500px"
-    >
-      <yandex-map-default-scheme-layer />
-      <yandex-map-default-features-layer />
-      <yandex-map-default-marker :settings="{ coordinates: markerCoords }" />
-    </yandex-map>
+<div class="container mx-auto py-8">
+    <h1 class="text-3xl font-bold mb-5">Доставка по Москве и области</h1>
     
-  </template>
-  
-  <script setup lang="ts">
-  import { ref, shallowRef } from 'vue';
-  import { YMap } from '@yandex/ymaps3-types';
-  
-  const map = shallowRef<null | YMap>(null);
-  
-  const mapState = ref({
-    center: [55.751574, 37.573856], 
-    zoom: 9, 
-  });
-  
-  const markerCoords = ref([55.751574, 37.573856]);
-  </script>
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <div class="bg-gray-100 p-6 rounded shadow">
+            <h2 class="text-xl font-semibold mb-2">Время работы службы доставки</h2>
+            <p>Пн–Вс 9:00–23:59</p>
+        </div>
+        
+        <div class="bg-gray-100 p-6 rounded shadow">
+            <h2 class="text-xl font-semibold mb-2">Способы оплаты</h2>
+            <p>Оплата на сайте (для физических лиц) или оплата по счету (для юридических лиц)</p>
+        </div>
+    </div>
+    <h1 class="text-3xl font-bold mb-5">Интервалы доставки</h1>
+    <p class="mb-5">Вы можете выбрать удобный для вас интервал времени доставки заказа</p>
+    
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <div class="p-4 bg-gray-100 rounded shadow">
+            <h2 class="text-xl font-semibold mb-3">В пределах МКАД</h2>
+            
+            <div class="mb-5">
+                <h3 class="font-semibold">На следующий день</h3>
+                <p>пн. - вс.:</p>
+                <ul>
+                    <li>09:00 - 15:00</li>
+                    <li>09:00 - 17:00</li>
+                    <li>10:00 - 15:00</li>
+                </ul>
+            </div>
+            
+            <div>
+                <h3 class="font-semibold">В день заказа</h3>
+                <p>пн. - пт.:</p>
+                <ul>
+                    <li>15:00 - 18:00</li>
+                    <li>18:00 - 23:59</li>
+                </ul>
+                <p>сб. - вс.:</p>
+                <ul>
+                    <li>18:00 - 22:00</li>
+                </ul>
+            </div>
+        </div>
+        <div class="p-4 bg-gray-100 rounded shadow">
+            <h2 class="text-xl font-semibold mb-3">За МКАД</h2>
+            
+            <div class="mb-5">
+                <h3 class="font-semibold">На следующий день</h3>
+                <p>пн. - вс.:</p>
+                <ul>
+                    <li>09:00 - 17:00</li>
+                </ul>
+            </div>
+            
+            <div>
+                <h3 class="font-semibold">В день заказа</h3>
+                <p>пн. - вс.:</p>
+                <ul>
+                    <li>18:00 - 23:59</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="p-4">
+        <div class="flex items-center">
+            <button @click="toggleMkadInside" class="mt-2 px-4 py-2 rounded hover:bg-gray-400">
+                Условие доставки
+            </button>
+            <img src="" alt="arrow" class="" />
+        </div>
+        <div v-if="isMkadInsideVisible" class="mt-4 p-4 bg-gray-100 border border-gray-300">
+            <p>Это содержимое внутри МКАД.</p>
+        </div>
+        
+        <div class="flex items-center">
+            <button @click="toggleMkadOutside" class="mt-2 px-4 py-2 rounded hover:bg-gray-400">
+                Памятка покупателя
+            </button>
+            <img src="" alt="arrow" class="" />
+        </div>
+        <div v-if="isMkadOutsideVisible" class="mt-4 p-4 bg-gray-100 border border-gray-300">
+            <p>Это содержимое вне МКАД.</p>
+        </div>
+    </div>
+
+</div>
+
+</template>
