4adeb6830d0c80042e75b30c73890804c4342cd2
Ma1n0s <kirill000e@gmail.com>
2025-02-20 15:37:48 +0500

    Моб.верстка

--- Frontend/vue-project/src/views/Courier-delivery.vue
+++ Frontend/vue-project/src/views/Courier-delivery.vue
@@ -15,7 +15,8 @@
   };
 </script>
 <template>
-<div class="container mx-auto py-8">
+    <Header />
+<div class="container mx-auto px-4 md:px-6 lg:px-8 py-8">
     <h2 class="text-3xl font-bold mb-5">Доставка по Москве и области</h2>
     
     <div class="bg-gray-100  p-4 max-w-md  my-4 rounded-lg">
@@ -108,6 +109,7 @@
             </div>
         </div>
     </div>
+    
     <div>
         <div class="flex flex-col w-full mt-2  hover:bg-gray-200 rounded">
             <hr class="border-gray-300" />
@@ -146,93 +148,96 @@
         <GMapMarker :position="{ lat: 10.0, lng: 10.0 }" />
       </GMapMap>
     </div> -->
-    <div>
-        <h2 class="text-2xl font-bold mb-4">Стоимость доставки, руб.</h2>
-        <table class="min-w-full border-collapse text-left text-sm ">
-            <thead class="">
-                <tr>
-                    <th class="px-6 py-3 border-b font-medium text-gray-700">Общий вес заказа</th>
-                    <th class="px-6 py-3 border-b font-medium text-gray-700">Внутри МКАД</th>
-                    <th class="px-6 py-3 border-b font-medium text-gray-700">От 0 до 15 км</th>
-                    <th class="px-6 py-3 border-b font-medium text-gray-700">От 16 до 35 км</th>
-                    <th class="px-6 py-3 border-b font-medium text-gray-700">От 36 до 60 км</th>
-                    <th class="px-6 py-3 border-b font-medium text-gray-700">От 61 до 100 км</th>
-                    <th class="px-6 py-3 border-b font-medium text-gray-700">От 101 до 130 км</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr class="border-b hover:bg-gray-50">
-                    <td class="px-6 py-4">от 0 до 14.9 кг</td>
-                    <td class="px-6 py-4">290</td>
-                    <td class="px-6 py-4">290</td>
-                    <td class="px-6 py-4">390</td>
-                    <td class="px-6 py-4">650</td>
-                    <td class="px-6 py-4">750</td>
-                    <td class="px-6 py-4">1000</td>
-                </tr>
-                <tr class="border-b hover:bg-gray-50">
-                    <td class="px-6 py-4">от 15 до 29.9 кг</td>
-                    <td class="px-6 py-4">540</td>
-                    <td class="px-6 py-4">540</td>
-                    <td class="px-6 py-4">640</td>
-                    <td class="px-6 py-4">900</td>
-                    <td class="px-6 py-4">1000</td>
-                    <td class="px-6 py-4">1250</td>
-                </tr>
-                <tr class="border-b hover:bg-gray-50">
-                    <td class="px-6 py-4">от 30 до 99.9 кг</td>
-                    <td class="px-6 py-4">780</td>
-                    <td class="px-6 py-4">780</td>
-                    <td class="px-6 py-4">1500</td>
-                    <td class="px-6 py-4">1800</td>
-                    <td class="px-6 py-4">2100</td>
-                    <td class="px-6 py-4">2400</td>
-                </tr>
-                <tr class="border-b hover:bg-gray-50">
-                    <td class="px-6 py-4">от 100 до 199.9 кг</td>
-                    <td class="px-6 py-4">1200</td>
-                    <td class="px-6 py-4">1400</td>
-                    <td class="px-6 py-4">1700</td>
-                    <td class="px-6 py-4">2000</td>
-                    <td class="px-6 py-4">2300</td>
-                    <td class="px-6 py-4">2600</td>
-                </tr>
-                <tr class="border-b hover:bg-gray-50">
-                    <td class="px-6 py-4">от 200 до 499.9 кг</td>
-                    <td class="px-6 py-4">1450</td>
-                    <td class="px-6 py-4">1650</td>
-                    <td class="px-6 py-4">1950</td>
-                    <td class="px-6 py-4">2250</td>
-                    <td class="px-6 py-4">2550</td>
-                    <td class="px-6 py-4">2850</td>
-                </tr>
-                <tr class="border-b hover:bg-gray-50">
-                    <td class="px-6 py-4">от 500 до 999.9 кг</td>
-                    <td class="px-6 py-4">1700</td>
-                    <td class="px-6 py-4">1900</td>
-                    <td class="px-6 py-4">2200</td>
-                    <td class="px-6 py-4">2500</td>
-                    <td class="px-6 py-4">2800</td>
-                    <td class="px-6 py-4">3100</td>
-                </tr>
-                <tr class="border-b hover:bg-gray-50">
-                    <td class="px-6 py-4">от 1000 до 1499.9 кг</td>
-                    <td class="px-6 py-4">9400</td>
-                    <td class="px-6 py-4">9800</td>
-                    <td class="px-6 py-4">10400</td>
-                    <td class="px-6 py-4">11100</td>
-                    <td class="px-6 py-4">12300</td>
-                    <td class="px-6 py-4">13200</td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
+    <div class="overflow-x-auto">
+    <h2 class="text-2xl font-bold mb-4">Стоимость доставки, руб.</h2>
+    <table class="min-w-full border-collapse text-left text-sm">
+        <thead>
+            <tr>
+                <th class="px-4 py-3 border-b font-medium text-gray-700">Общий вес заказа</th>
+                <th class="px-4 py-3 border-b font-medium text-gray-700">Внутри МКАД</th>
+                <th class="px-4 py-3 border-b font-medium text-gray-700 hidden md:table-cell">От 0 до 15 км</th>
+                <th class="px-4 py-3 border-b font-medium text-gray-700 hidden md:table-cell">От 16 до 35 км</th>
+                <th class="px-4 py-3 border-b font-medium text-gray-700 hidden md:table-cell">От 36 до 60 км</th>
+                <th class="px-4 py-3 border-b font-medium text-gray-700 hidden md:table-cell">От 61 до 100 км</th>
+                <th class="px-4 py-3 border-b font-medium text-gray-700 hidden md:table-cell">От 101 до 130 км</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="border-b hover:bg-gray-50">
+                <td class="px-4 py-4">от 0 до 14.9 кг</td>
+                <td class="px-4 py-4">290</td>
+                <td class="px-4 py-4 hidden md:table-cell">290</td>
+                <td class="px-4 py-4 hidden md:table-cell">390</td>
+                <td class="px-4 py-4 hidden md:table-cell">650</td>
+                <td class="px-4 py-4 hidden md:table-cell">750</td>
+                <td class="px-4 py-4 hidden md:table-cell">1000</td>
+            </tr>
+            <tr class="border-b hover:bg-gray-50">
+                <td class="px-4 py-4">от 15 до 29.9 кг</td>
+                <td class="px-4 py-4">540</td>
+                <td class="px-4 py-4 hidden md:table-cell">540</td>
+                <td class="px-4 py-4 hidden md:table-cell">640</td>
+                <td class="px-4 py-4 hidden md:table-cell">900</td>
+                <td class="px-4 py-4 hidden md:table-cell">1000</td>
+                <td class="px-4 py-4 hidden md:table-cell">1250</td>
+            </tr>
+            <tr class="border-b hover:bg-gray-50">
+                <td class="px-4 py-4">от 30 до 99.9 кг</td>
+                <td class="px-4 py-4">780</td>
+                <td class="px-4 py-4 hidden md:table-cell">780</td>
+                <td class="px-4 py-4 hidden md:table-cell">1500</td>
+                <td class="px-4 py-4 hidden md:table-cell">1800</td>
+                <td class="px-4 py-4 hidden md:table-cell">2100</td>
+                <td class="px-4 py-4 hidden md:table-cell">2400</td>
+            </tr>
+            <tr class="border-b hover:bg-gray-50">
+                <td class="px-4 py-4">от 100 до 199.9 кг</td>
+                <td class="px-4 py-4">1200</td>
+                <td class="px-4 py-4 hidden md:table-cell">1400</td>
+                <td class="px-4 py-4 hidden md:table-cell">1700</td>
+                <td class="px-4 py-4 hidden md:table-cell">2000</td>
+                <td class="px-4 py-4 hidden md:table-cell">2300</td>
+                <td class="px-4 py-4 hidden md:table-cell">2600</td>
+            </tr>
+            <tr class="border-b hover:bg-gray-50">
+                <td class="px-4 py-4">от 200 до 499.9 кг</td>
+                <td class="px-4 py-4">1450</td>
+                <td class="px-4 py-4 hidden md:table-cell">1650</td>
+                <td class="px-4 py-4 hidden md:table-cell">1950</td>
+                <td class="px-4 py-4 hidden md:table-cell">2250</td>
+                <td class="px-4 py-4 hidden md:table-cell">2550</td>
+                <td class="px-4 py-4 hidden md:table-cell">2850</td>
+            </tr>
+            <tr class="border-b hover:bg-gray-50">
+                <td class="px-4 py-4">от 500 до 999.9 кг</td>
+                <td class="px-4 py-4">1700</td>
+                <td class="px-4 py-4 hidden md:table-cell">1900</td>
+                <td class="px-4 py-4 hidden md:table-cell">2200</td>
+                <td class="px-4 py-4 hidden md:table-cell">2500</td>
+                <td class="px-4 py-4 hidden md:table-cell">2800</td>
+                <td class="px-4 py-4 hidden md:table-cell">3100</td>
+            </tr>
+            <tr class="border-b hover:bg-gray-50">
+                <td class="px-4 py-4">от 1000 до 1499.9 кг</td>
+                <td class="px-4 py-4">9400</td>
+                <td class="px-4 py-4 hidden md:table-cell">9800</td>
+                <td class="px-4 py-4 hidden md:table-cell">10400</td>
+                <td class="px-4 py-4 hidden md:table-cell">11100</td>
+                <td class="px-4 py-4 hidden md:table-cell">12300</td>
+                <td class="px-4 py-4 hidden md:table-cell">13200</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+
+
+
     <div>
         <div>
             <h2 class="text-2xl font-bold mb-4">Подъем заказа</h2>
             <p class="text-gray-700 mb-4">Наш магазин предлагает дополнительные услуги подъема заказа на этаж. Подъем осуществляется на лифте либо пешком, но не выше 5-го этажа. Условия и стоимость услуги зависят от веса товара.
             </p>
-            <p class="text-gray-700">Если заказ имеет вес тяжелее 1000 кг - то в таком случае стоимость подъема согласовывается отдельно.</p>
+            <p class="text-gray-700 text-sm">Если заказ имеет вес тяжелее 1000 кг - то в таком случае стоимость подъема согласовывается отдельно.</p>
         </div>
         <div>
             <h2 class="text-2xl font-bold mb-4">Подъём заказа</h2>
@@ -248,7 +253,7 @@
         </div>
     <div>
         <h2 class="text-2xl font-bold mb-4">От 30 до 1000 кг – подъем осуществляет грузчик</h2>
-        <p>Услуги грузчика предоставляются только в пределах МКАД с 10:00 до 18:00 по будням. Оформить заявку на услугу можно до 15:00 дня, предшествующего дате доставки. Общий вес заказа не должен превышать 1000 кг, а вес каждого товара – 35 кг.</p>
+        <p >Услуги грузчика предоставляются только в пределах МКАД с 10:00 до 18:00 по будням. Оформить заявку на услугу можно до 15:00 дня, предшествующего дате доставки. Общий вес заказа не должен превышать 1000 кг, а вес каждого товара – 35 кг.</p>
     </div>
     <div class="w-1/2">
         <h2 class="text-2xl font-bold mb-4">Стоимость доставки, руб.</h2>
@@ -299,7 +304,7 @@
 
 
 </div>
-
+<Footer />
 </template>
 
 <style>
