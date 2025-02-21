b860738f2e301ed68176c302911d3da0ed3e1f68
Ma1n0s <kirill000e@gmail.com>
2025-02-21 19:58:18 +0500

    Комментирование

--- Frontend/vue-project/src/main.js
+++ Frontend/vue-project/src/main.js
@@ -3,17 +3,18 @@ import './assets/main.css';
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-import { loadGoogleMaps } from '@fawmi/vue-google-maps';
+import { equal } from 'fast-deep-equal';
+// import { loadGoogleMaps } from '@fawmi/vue-google-maps';
 import './assets/tailwind.css';
 
 const app = createApp(App);
 
-app.use(loadGoogleMaps, {
-    load: {
-      key: 'YOUR_API_KEY',
-      libraries: 'places', 
-    },
-  });
+// app.use(loadGoogleMaps, {
+//     load: {
+//       key: 'YOUR_API_KEY',
+//       libraries: 'places', 
+//     },
+//   });
 
 app.use(router);
 app.mount('#app');
