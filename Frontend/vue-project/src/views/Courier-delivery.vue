9f19a4516381b725a98154f4e7007a3a8a5bffc2
Ma1n0s <kirill000e@gmail.com>
2025-02-21 16:28:39 +0500

    фикс routeLink

--- Frontend/vue-project/src/views/Courier-delivery.vue
+++ Frontend/vue-project/src/views/Courier-delivery.vue
@@ -291,13 +291,13 @@
         <div class="bg-gray-100 p-6 rounded-2xl shadow">
         <h2 class="text-lg font-semibold">Доставка транспортной компанией</h2>
         <p class="text-gray-700">Вы сможете заказать доставку транспортными компаниями «Почта России», «СДЭК».</p>
-        <RouterLink class="text-blue-600 hover:underline mt-2">Полные условия программы</RouterLink>
+        <RouterLink to="" class="text-blue-600 hover:underline mt-2">Полные условия программы</RouterLink>
     </div>
 
     <div class="bg-gray-100 p-6 rounded-2xl shadow">
         <h2 class="text-lg font-semibold">Отследите ваш заказ</h2>
         <p class="text-gray-700">Воспользуйтесь проверкой статуса заказа. Если у вас возникли вопросы по срокам или стоимости доставки, обращайтесь к нашим менеджерам по телефону 8 800 550-37-70.</p>
-        <RouterLink href="#" class="text-blue-600 hover:underline mt-2">Проверить статус заказа</RouterLink>
+        <RouterLink to="" class="text-blue-600 hover:underline mt-2">Проверить статус заказа</RouterLink>
         </div>
     </div>
 
