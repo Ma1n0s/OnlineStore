160d0aadbcc2c3d54fe59569f8b861830fe33869
Ma1n0s <kirill000e@gmail.com>
2025-03-05 05:56:31 +0500

    доб блоки

--- Frontend/vue-project/src/views/Main.vue
+++ Frontend/vue-project/src/views/Main.vue
@@ -24,6 +24,34 @@ const prev = () => {
 <template>
 <Header/>
   <div class="container mx-auto px-4 md:px-6 lg:px-8 py-8">
+
+    <div class="flex gap-4 mt-4"> 
+            <div class="bg-gray-100 flex flex-col space-y-2 justify-between rounded-lg p-4 w-52 h-full hover:bg-gray-300 transition duration-200 ease-in-out shadow-md">
+                <div class="font-bold mb-4">Инструмент</div>
+                <img src="/public/15.webp" alt="alt" class="w-52 h-32 object-cover rounded-md" />
+            </div>
+            <div class="bg-gray-100 flex flex-col space-y-2 justify-between rounded-lg p-4 w-52 h-full hover:bg-gray-300 transition duration-200 ease-in-out shadow-md">
+                <div class="font-bold mb-4">Электрика</div>
+                <img src="/public/15.webp" alt="alt" class="w-52 h-32 object-cover rounded-md" />
+            </div>
+            <div class="bg-gray-100 flex flex-col space-y-2 justify-between rounded-lg p-4 w-52 h-full hover:bg-gray-300 transition duration-200 ease-in-out shadow-md">
+                <div class="font-bold mb-4">Крепеж</div>
+                <img src="/public/15.webp" alt="alt" class="w-52 h-32 object-cover rounded-md" />
+            </div>
+            <div class="bg-gray-100 flex flex-col space-y-2 justify-between rounded-lg p-4 w-52 h-full hover:bg-gray-300 transition duration-200 ease-in-out shadow-md">
+                <div class="font-bold mb-4">Силовая техника</div>
+                <img src="/public/15.webp" alt="alt" class="w-52 h-32 object-cover rounded-md" />
+            </div>
+            <div class="bg-gray-100 flex flex-col space-y-2 justify-between rounded-lg p-4 w-52 h-full hover:bg-gray-300 transition duration-200 ease-in-out shadow-md">
+                <div class="font-bold mb-4">Ручной инструмент</div>
+                <img src="/public/15.webp" alt="alt" class="w-52 h-32 object-cover rounded-md" />
+            </div>
+            <div class="bg-gray-100 flex flex-col space-y-2 justify-between rounded-lg p-4 w-52 h-full hover:bg-gray-300 transition duration-200 ease-in-out shadow-md">
+                <div class="font-bold mb-4">Станки</div>
+                <img src="/public/15.webp" alt="alt" class="w-52 h-32 object-cover rounded-md" />
+            </div>
+        </div>
+
     <div class="relative overflow-hidden">
       <div class="flex transition-transform duration-500" :style="{ transform: `translateX(-${currentIndex * 100}%)` }">
         <div v-for="(item, index) in items" :key="index" class="bg-white rounded-lg shadow-lg max-w-xs flex-shrink-0">
