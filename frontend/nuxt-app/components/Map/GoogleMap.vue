716c005313d1886c9bd6b22555727375c59aa918
Ma1n0s <kirill000e@g,ail.com>
2025-03-11 15:08:03 +0500

    +GoogleMap

---
+++ frontend/nuxt-app/components/Map/GoogleMap.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+import { Loader } from '@googlemaps/js-api-loader';
+
+const map = ref(null); 
+
+onMounted(async () => {
+  const loader = new Loader({
+    apiKey: '', // добавь сюда свой ключ
+    version: 'weekly',
+  });
+
+  await loader.load();
+
+
+  new google.maps.Map(map.value, {
+    center: { lat: 59.934280, lng: 30.335098 }, 
+    zoom: 12, 
+  });
+});
+</script>
+<template>
+    <div ref="map" class="w-full h-96"></div>
+</template>
+  
+
+
+  
+ 
--- frontend/nuxt-app/pages/index.vue
+++ frontend/nuxt-app/pages/index.vue
@@ -3,6 +3,7 @@ import { ref } from 'vue';
 import Footer from '~/components/Footer/Footer.vue';
 import Modal from '~/components/Modal/Modal.vue';
 import Button from '~/shared/ui/Button/Button.vue';
+import GoogleMap from '~/components/Map/GoogleMap.vue';
 
 const isModalOpen = ref(false);
 
@@ -28,6 +29,10 @@ const handleConfirm = () => {
     <Modal :isOpen="isModalOpen" @close="closeModal" @confirm="handleConfirm" title="">
       <p>Текст</p>
     </Modal>
+  </div>
+  <div>
+    <h1 class="text-2xl font-bold mb-4">Google Map</h1>
+    <GoogleMap />
   </div>
 </div>
   <Footer />
