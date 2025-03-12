3455e814c3615fe9cdf5accf46bbc8234e2eac04
Nnder <asfahaikrkt6k56@yandex.ru>
2025-03-12 03:25:14 +0300

    Добавил стандартные цвета в конфиг

--- frontend/nuxt-app/tailwind.config.js
+++ frontend/nuxt-app/tailwind.config.js
@@ -12,7 +12,20 @@ export default {
     fontSize,
     extend: {
       colors: {
-        primary: colors.green,
+        primary: {
+          DEFAULT: "#89CFF0",
+          hover: "#60BBCB",
+          active: "#60BBCB",
+        },
+        second: {
+          DEFAULT: "#1B998B",
+          hover: "#31918C",
+          active: "#31918C",
+        },
+        white: "#E9F1F7",
+        danger: "#D62828",
+        dark: "#0D1B2A",
+        gray: "#70798C",
       },
     },
   },
