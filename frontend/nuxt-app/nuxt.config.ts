8d6f26695b0a3981e675479db0e54986842ae008
Ma1n0s <kirill000e@g,ail.com>
2025-03-13 23:46:50 +0500

    +process.env.GOOGLE_MAPS_API_KEY

--- frontend/nuxt-app/nuxt.config.ts
+++ frontend/nuxt-app/nuxt.config.ts
@@ -3,4 +3,10 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt"],
+
+  runtimeConfig: {
+    public: {
+      googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY, 
+    },
+  },
 });
