40bf90c4d9eb4d1f34cb726f7045840f4475c75d
Nnder <asfahaikrkt6k56@yandex.ru>
2025-03-15 05:48:31 +0300

    Laravel авторизация, временный пользователь

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Console\Scheduling\Schedule;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
+            return config('app.frontend_url')."/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
+        });
+
+        $this->app->booted(function () {
+            $schedule = $this->app->make(Schedule::class);
+            
+            // Добавьте ваши задачи здесь
+            $schedule->command('guests:delete-expired')->daily();
+        });
     }
 }
--- backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,12 +13,19 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name')->nullable();
+            $table->string('phone')->unique()->nullable();
+            $table->string('email')->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
+            $table->boolean('is_guest')->default(false);
             $table->rememberToken();
             $table->timestamps();
+            $table->timestamp('expires_at')->nullable();
+
+            $table->index('email');
+            $table->index('phone');
+            $table->index('is_guest');
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
