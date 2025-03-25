e042205faccfdc034bc2a5dcfd07e08baeadf22f
Nnder <asfahaikrkt6k56@yandex.ru>
2025-03-25 07:01:30 +0300

    цвет фона на главной странице

--- frontend/nuxt-app/pages/index.vue
+++ frontend/nuxt-app/pages/index.vue
@@ -20,7 +20,7 @@ const handleConfirm = () => {
 const s = ref();
 </script>
 <template>
-  <div class="bg-gray-800 w-full ~text-sm/xs ~p-0/80">
+  <div class="bg-white w-full ~text-sm/xs ~p-0/80">
     <div>
       <button @click="openModal" class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
         Открыть модальное окно
