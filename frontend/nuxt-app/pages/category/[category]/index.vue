074b34831d837ce709be78b60a91ba16e2f386e9
Nnder <asfawga2add@yandex.ru>
2025-04-07 13:14:18 +0500

    swiper + catalog

--- frontend/nuxt-app/pages/category/[category]/index.vue
+++ frontend/nuxt-app/pages/category/[category]/index.vue
@@ -106,24 +106,7 @@ const state = reactive({
         <NuxtLink to="#" class="text-primary hover:underline mt-2 md:mt-0">Как выбрать электроинструмент</NuxtLink>
       </div>
 
-      <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4 mb-8">
-        <NuxtLink
-          v-for="(product, index) in state.products"
-          :key="index"
-          to="/category/instuments/wire"
-          class="bg-gray-100 flex flex-col justify-between rounded-lg p-4 h-full hover:bg-gray-300 transition duration-200 ease-in-out shadow-md"
-        >
-          <div class="font-medium">{{ product.name }}</div>
-          <NuxtImg
-            :src="product.image"
-            :alt="product.name"
-            class="self-end mt-2 object-contain"
-            width="40"
-            height="40"
-            format="webp"
-          />
-        </NuxtLink>
-      </div>
+      <CategoryList class="mb-8" />
 
       <div class="mb-8">
         <h2 class="text-xl font-semibold mb-4">Часто ищут</h2>
--- frontend/nuxt-app/pages/index.vue
+++ frontend/nuxt-app/pages/index.vue
@@ -2,10 +2,22 @@
 import BrandSwiper from "~/components/Swiper/BrandSwiper.vue";
 import CategorySwiper from "~/components/Swiper/CategorySwiper.vue";
 import ProductSwiper from "~/components/Swiper/ProductSwiper/ProductSwiper.vue";
+import BigSwiper from "~/components/Swiper/BigSwiper.vue";
+import CategoryList from "~/components/CategoryList/CategoryList.vue";
 </script>
 <template>
   <div class="bg-white w-full ~text-sm/xs ~p-4/8 flex flex-col items-center">
     <div class="w-full xl:max-w-7xl">
+      <div class="mb-16">
+        <BigSwiper />
+      </div>
+
+      <div class="mb-16">
+        <WrapperHeader title="Кататегории товаров">
+          <CategoryList />
+        </WrapperHeader>
+      </div>
+
       <div class="mb-16">
         <WrapperHeader title="Кататегории товаров">
           <CategorySwiper />
