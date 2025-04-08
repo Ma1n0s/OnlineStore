e413dd5a89bf99a5e8f994d73d731f28e05d8b92
Kirill <kirill000e@gmail.com>
2025-04-08 11:44:07 +0500

    fix

--- frontend/nuxt-app/shared/ui/FormField/AuthForm.vue
+++ frontend/nuxt-app/shared/ui/FormField/AuthForm.vue
@@ -9,7 +9,7 @@ const isRegister = ref(false)
 
 const currentFormComponent = computed(() => {
 	if (isRegister.value) return RegisterForm
-	return authType.value === 'login' ? LoginForm : PhoneLoginForm
+	return authType.value === 'login' ? PhoneLoginForm : LoginForm
 })
 
 const switchToRegister = () => {
@@ -23,18 +23,7 @@ const switchToLogin = () => {
 
 <template>
 	<div class="mx-auto p-8 bg-white rounded-2xl shadow-sm">
-		<div v-if="!isRegister" class="relative mb-8 flex bg-gray-100 p-1 rounded-lg">
-			<button
-				@click="authType = 'login'"
-				class="flex-1 py-2 px-4 text-sm font-medium text-center transition-colors"
-				:class="{
-					'text-primary bg-white rounded-md shadow-xs': authType === 'login',
-					'text-gray-500 hover:text-gray-700': authType !== 'login',
-				}"
-			>
-				Обычный вход
-			</button>
-
+		<div class="relative mb-8 flex bg-gray-100 p-1 rounded-lg">
 			<button
 				@click="authType = 'phone'"
 				class="flex-1 py-2 px-4 text-sm font-medium text-center transition-colors"
@@ -45,10 +34,17 @@ const switchToLogin = () => {
 			>
 				Вход по коду
 			</button>
+			<button
+				@click="authType = 'login'"
+				class="flex-1 py-2 px-4 text-sm font-medium text-center transition-colors"
+				:class="{
+					'text-primary bg-white rounded-md shadow-xs': authType === 'login',
+					'text-gray-500 hover:text-gray-700': authType !== 'login',
+				}"
+			>
+				Обычный вход
+			</button>
 		</div>
-
-		<h2 v-if="isRegister" class="text-2xl font-bold text-center mb-6">Регистрация</h2>
-		<h2 v-else class="text-2xl font-bold text-center mb-6">Вход в аккаунт</h2>
 
 		<component :is="currentFormComponent" @switch-to-register="switchToRegister" @switch-to-login="switchToLogin" />
 	</div>
