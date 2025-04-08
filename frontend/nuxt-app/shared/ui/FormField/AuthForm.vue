37b46daee8b435c0c20eb33c293623d30097dbd1
Kirill <kirill000e@gmail.com>
2025-04-08 11:47:36 +0500

    fix

--- frontend/nuxt-app/shared/ui/FormField/AuthForm.vue
+++ frontend/nuxt-app/shared/ui/FormField/AuthForm.vue
@@ -25,21 +25,21 @@ const switchToLogin = () => {
 	<div class="mx-auto p-8 bg-white rounded-2xl shadow-sm">
 		<div class="relative mb-8 flex bg-gray-100 p-1 rounded-lg">
 			<button
-				@click="authType = 'phone'"
-				class="flex-1 py-2 px-4 text-sm font-medium text-center transition-colors"
-				:class="{
-					'text-primary bg-white rounded-md shadow-xs': authType === 'phone',
-					'text-gray-500 hover:text-gray-700': authType !== 'phone',
-				}"
-			>
-				Вход по коду
-			</button>
-			<button
 				@click="authType = 'login'"
 				class="flex-1 py-2 px-4 text-sm font-medium text-center transition-colors"
 				:class="{
 					'text-primary bg-white rounded-md shadow-xs': authType === 'login',
 					'text-gray-500 hover:text-gray-700': authType !== 'login',
+				}"
+			>
+				Вход по коду
+			</button>
+			<button
+				@click="authType = 'phone'"
+				class="flex-1 py-2 px-4 text-sm font-medium text-center transition-colors"
+				:class="{
+					'text-primary bg-white rounded-md shadow-xs': authType === 'phone',
+					'text-gray-500 hover:text-gray-700': authType !== 'phone',
 				}"
 			>
 				Обычный вход
