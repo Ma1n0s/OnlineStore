b150b6c127dda7878dadfb76d62ed789330f44ce
Nnder <asfahaikrkt6k56@yandex.ru>
2025-04-08 12:00:22 +0500

    reset

--- frontend/nuxt-app/shared/ui/FormField/AuthForm.vue
+++ frontend/nuxt-app/shared/ui/FormField/AuthForm.vue
@@ -2,23 +2,12 @@
 import { ref, computed } from 'vue'
 import LoginForm from './LoginForm.vue'
 import PhoneLoginForm from './PhoneLoginForm.vue'
-import RegisterForm from './RegisterForm.vue'
 
 const authType = ref('login')
-const isRegister = ref(false)
 
 const currentFormComponent = computed(() => {
-	if (isRegister.value) return RegisterForm
-	return authType.value === 'login' ? PhoneLoginForm : LoginForm
+	return authType.value === 'login' ? LoginForm : PhoneLoginForm
 })
-
-const switchToRegister = () => {
-	isRegister.value = true
-}
-
-const switchToLogin = () => {
-	isRegister.value = false
-}
 </script>
 
 <template>
@@ -32,8 +21,9 @@ const switchToLogin = () => {
 					'text-gray-500 hover:text-gray-700': authType !== 'login',
 				}"
 			>
-				Вход по коду
+				Обычный вход
 			</button>
+
 			<button
 				@click="authType = 'phone'"
 				class="flex-1 py-2 px-4 text-sm font-medium text-center transition-colors"
@@ -42,10 +32,10 @@ const switchToLogin = () => {
 					'text-gray-500 hover:text-gray-700': authType !== 'phone',
 				}"
 			>
-				Обычный вход
+				Вход по коду
 			</button>
 		</div>
 
-		<component :is="currentFormComponent" @switch-to-register="switchToRegister" @switch-to-login="switchToLogin" />
+		<component :is="currentFormComponent" />
 	</div>
 </template>
