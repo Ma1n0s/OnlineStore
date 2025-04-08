c40416ccf8297e6f4b3bc99a88d53b76372dcb5b
Kirill <kirill000e@gmail.com>
2025-04-08 17:41:33 +0500

    Доб закрытие обертки, изм иконки

--- frontend/nuxt-app/components/Modal/Modal.vue
+++ frontend/nuxt-app/components/Modal/Modal.vue
@@ -20,15 +20,26 @@ const closeModal = () => {
 const confirmAction = () => {
 	emit('confirm')
 }
+
+// Функция для закрытия при клике на оверлей
+const handleOverlayClick = event => {
+	if (event.target === event.currentTarget) {
+		closeModal()
+	}
+}
 </script>
 
 <template>
-	<div v-if="isOpen" class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+	<div
+		v-if="isOpen"
+		class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50"
+		@click="handleOverlayClick"
+	>
 		<div class="bg-white rounded-lg shadow-lg w-full h-full md:w-11/12 md:h-auto md:max-w-md lg:max-w-lg p-6">
 			<div class="flex justify-between items-center mb-4">
 				<h3 class="text-xl font-semibold">{{ title }}</h3>
-				<button @click="closeModal" class="text-gray-500 hover:text-gray-700">
-					<Icon name="tabler:box" class="w-7 h-7" />
+				<button @click="closeModal" class="text-gray-500 hover:text-primary-hover transition-colors">
+					<Icon name="tabler:xbox-x" class="w-7 h-7" />
 				</button>
 			</div>
 			<div class="mb-4">
