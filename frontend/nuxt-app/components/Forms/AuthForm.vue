cb2281a8bc10b174cd82170fc6407829056fa27f
Nnder <asfahaikrkt6k56@yandex.ru>
2025-04-09 16:59:35 +0500

    prepare

--- frontend/nuxt-app/components/Forms/AuthForm.vue
+++ frontend/nuxt-app/components/Forms/AuthForm.vue
@@ -1,22 +1,22 @@
 <script setup>
-import { ref, computed } from "vue";
-import LoginForm from "./LoginForm.vue";
-import PhoneLoginForm from "./EmailLoginForm.vue";
+import { ref, computed } from 'vue'
+import LoginForm from './LoginForm.vue'
+import PhoneLoginForm from './EmailLoginForm.vue'
 
-const authType = ref("email");
+const authType = ref('email')
 
-const emit = defineEmits(["close"]);
+const emit = defineEmits(['close'])
 
 const currentFormComponent = computed(() => {
-  return authType.value === "login" ? LoginForm : PhoneLoginForm;
-});
+  return authType.value === 'login' ? LoginForm : PhoneLoginForm
+})
 </script>
 
 <template>
   <div class="mx-auto p-4 bg-white rounded-2xl shadow-sm">
     <div class="flex justify-between">
       <div class="text-xl font-bold text-center w-full">
-        {{ authType === "email" ? "Вход / Регистрация" : "Вход по паролю" }}
+        {{ authType === 'email' ? 'Вход / Регистрация' : 'Вход по паролю' }}
       </div>
     </div>
     <div class="relative mb-4 flex bg-gray-100 p-1 rounded-lg">
