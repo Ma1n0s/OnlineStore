14406fa9eaadf026b2327bc99126339996fb512f
Kirill <kirill000e@gmail.com>
2025-04-10 15:38:18 +0500

    Создание страницы статьей/news

---
+++ frontend/nuxt-app/pages/news/news.vue
@@ -0,0 +1,83 @@
+// можно без обертки чтобы были элементы title картинка и текст
+<script setup>
+import ArticleCard from '~/components/News/ArticleCard.vue'
+import EditorsPick from '~/components/News/EditorsPick.vue'
+const articles = [
+  {
+    id: 1,
+    title: 'Создание странички',
+    excerpt: 'Небольшой тестовый текст по проверка внешнего вида.',
+    category: 'Статья',
+    date: 'Апреля 10, 2025',
+    image:
+      'https://images.unsplash.com/photo-1522071820081-009f0129c71c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
+    author: 'Кирилл Ф',
+  },
+  {
+    id: 2,
+    title: 'Создание странички',
+    excerpt: 'Небольшой тестовый текст по проверка внешнего вида.',
+    category: 'Статья',
+    date: 'Апреля 10, 2025',
+    image:
+      'https://images.unsplash.com/photo-1522071820081-009f0129c71c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
+    author: 'Кирилл Ф',
+  },
+  {
+    id: 3,
+    title: 'Создание странички',
+    excerpt: 'Небольшой тестовый текст по проверка внешнего вида.',
+    category: 'Статья',
+    date: 'Апреля 10, 2025',
+    image:
+      'https://images.unsplash.com/photo-1522071820081-009f0129c71c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
+    author: 'Кирилл Ф',
+  },
+  {
+    id: 4,
+    title: 'Создание странички',
+    excerpt: 'Небольшой тестовый текст по проверка внешнего вида.',
+    category: 'Статья',
+    date: 'Апреля 10, 2025',
+    image:
+      'https://images.unsplash.com/photo-1522071820081-009f0129c71c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
+    author: 'Кирилл Ф',
+  },
+  {
+    id: 5,
+    title: 'Создание странички',
+    excerpt: 'Небольшой тестовый текст по проверка внешнего вида.',
+    category: 'Статья',
+    date: 'Апреля 10, 2025',
+    image:
+      'https://images.unsplash.com/photo-1522071820081-009f0129c71c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
+    author: 'Кирилл Ф',
+  },
+  {
+    id: 6,
+    title: 'Создание странички',
+    excerpt: 'Небольшой тестовый текст по проверка внешнего вида.',
+    category: 'Статья',
+    date: 'Апреля 10, 2025',
+    image:
+      'https://images.unsplash.com/photo-1522071820081-009f0129c71c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
+    author: 'Кирилл Ф',
+  },
+]
+</script>
+
+<template>
+  <div class="min-h-screen bg-gray-50">
+    <div class="container mx-auto px-4 py-8">
+      <div class="grid grid-cols-1 mx-6 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <ArticleCard
+          v-for="article in articles"
+          :key="article.id"
+          :article="article"
+          class="hover:scale-105 transition-transform duration-200"
+        />
+      </div>
+      <EditorsPick />
+    </div>
+  </div>
+</template>
