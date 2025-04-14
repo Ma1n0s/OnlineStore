719808020a9f04b2dd9abd717b593062c99c3de5
Nnder <asfahaikrkt6k56@yandex.ru>
2025-04-14 11:44:10 +0500

    Компонент преимуществ

---
+++ frontend/nuxt-app/components/Advantages/Advantages.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="flex flex-col lg:flex-row items-center lg:flex-wrap justify-evenly gap-16 pt-8">
+    <div class="flex flex-col gap-2 border-r border-primary w-full px-4 lg:w-1/3">
+      <div class="flex items-center gap-2 text-lg sm:text-xl">
+        <Icon name="material-symbols:check-circle-outline-rounded" class="h-6 w-6" />
+        <p class="font-extrabold">Гарантия качества</p>
+      </div>
+      <p class="text-base sm:text-lg">Мы гарантируем качество нашей продукции и сервиса.</p>
+    </div>
+
+    <div class="flex flex-col gap-2 border-r border-primary w-full px-4 lg:w-1/3">
+      <div class="flex items-center gap-2 text-lg sm:text-xl">
+        <Icon name="material-symbols:star-half-rounded" class="h-6 w-6" />
+        <p class="font-extrabold">Уникальные предложения</p>
+      </div>
+      <p class="text-base sm:text-lg">
+        У нас есть уникальные товары, которые вы не найдете ни в одном другом магазине.
+      </p>
+    </div>
+
+    <div class="flex flex-col gap-2 border-r border-primary w-full px-4 lg:w-1/3">
+      <div class="flex items-center gap-2 text-lg sm:text-xl">
+        <Icon name="material-symbols:passkey-rounded" class="h-6 w-6" />
+        <p class="font-extrabold">Аренда оборудования</p>
+      </div>
+      <p class="text-base sm:text-lg">Аренда оборудования на выгодных условиях.</p>
+    </div>
+
+    <div class="flex flex-col gap-2 border-r border-primary w-full px-4 lg:w-1/3">
+      <div class="flex items-center gap-2 text-lg sm:text-xl">
+        <Icon name="material-symbols:check-circle-outline-rounded" class="h-6 w-6" />
+        <p class="font-extrabold">Аренда оборудования</p>
+      </div>
+      <p class="text-base sm:text-lg">Аренда оборудования на выгодных условиях.</p>
+    </div>
+  </div>
+</template>
