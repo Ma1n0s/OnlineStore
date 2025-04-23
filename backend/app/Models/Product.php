371b06826fea9bdcf73c3f7f212768be8c6d1090
Nnder <asfahaikrkt6k56@yandex.ru>
2025-04-23 11:22:39 +0500

    category id

--- backend/app/Models/Product.php
+++ backend/app/Models/Product.php
@@ -29,7 +29,7 @@ class Product extends Model
         'slug',
         'brand',
         'rating',
-        // 'category_id',
+        'category_id',
         'subcategory_id',
         'specifications',
         'images',
