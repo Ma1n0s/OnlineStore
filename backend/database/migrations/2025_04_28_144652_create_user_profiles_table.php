31be2e138b55d9dc77d41823d71b7b8a869391d3
Ma1n0s <kirill000e@gmail.com>
2025-04-28 20:05:06 +0500

    доб таблицы

---
+++ backend/database/migrations/2025_04_28_144652_create_user_profiles_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up()
+    {
+        Schema::create('user_profiles', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            
+            $table->string('last_name')->nullable();
+            $table->string('patronymic')->nullable();
+            
+            $table->string('company_name')->nullable();
+            $table->string('inn')->nullable();
+            $table->string('kpp')->nullable();
+            $table->text('legal_address')->nullable();
+            
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('user_profiles');
+    }
+};
