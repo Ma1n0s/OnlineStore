0e5ef78b59728f99df879f5bccb7d5182f37f6d8
Nnder <asfahaikrkt6k56@yandex.ru>
2025-05-07 01:58:04 +0300

    Переделал новости

--- frontend/nuxt-app/pages/news/[id].vue
+++ frontend/nuxt-app/pages/news/[id].vue
@@ -3,42 +3,42 @@ const route = useRoute()
 const { id } = route.params
 
 const articles = [
-  // {
-  //   id: 1,
-  //   title: 'Создание странички',
-  //   content: 'Полный текст статьи 1...',
-  //   excerpt: 'Небольшой тестовый текст по проверка внешнего вида.',
-  //   date: 'Апреля 10, 2025',
-  //   image:
-  //     'https://images.unsplash.com/photo-1522071820081-009f0129c71c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
-  // },
-  // {
-  //   id: 2,
-  //   title: 'Создание странички',
-  //   content: 'Полный текст статьи 2...',
-  //   excerpt: 'Небольшой тестовый текст по проверка внешнего вида.',
-  //   date: 'Апреля 10, 2025',
-  //   image:
-  //     'https://images.unsplash.com/photo-1522071820081-009f0129c71c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
-  // },
-  // {
-  //   id: 3,
-  //   title: 'Создание странички',
-  //   content: 'Полный текст статьи 3...',
-  //   excerpt: 'Небольшой тестовый текст по проверка внешнего вида.',
-  //   date: 'Апреля 10, 2025',
-  //   image:
-  //     'https://images.unsplash.com/photo-1522071820081-009f0129c71c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
-  // },
-  // {
-  //   id: 4,
-  //   title: 'Создание странички',
-  //   content: 'Полный текст статьи 4...',
-  //   excerpt: 'Небольшой тестовый текст по проверка внешнего вида.',
-  //   date: 'Апреля 10, 2025',
-  //   image:
-  //     'https://images.unsplash.com/photo-1522071820081-009f0129c71c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
-  // },
+  {
+    id: 1,
+    title: 'Создание странички',
+    content: 'Полный текст статьи 1...',
+    excerpt: 'Небольшой тестовый текст по проверка внешнего вида.',
+    date: 'Апреля 10, 2025',
+    image:
+      'https://images.unsplash.com/photo-1522071820081-009f0129c71c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
+  },
+  {
+    id: 2,
+    title: 'Создание странички',
+    content: 'Полный текст статьи 2...',
+    excerpt: 'Небольшой тестовый текст по проверка внешнего вида.',
+    date: 'Апреля 10, 2025',
+    image:
+      'https://images.unsplash.com/photo-1522071820081-009f0129c71c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
+  },
+  {
+    id: 3,
+    title: 'Создание странички',
+    content: 'Полный текст статьи 3...',
+    excerpt: 'Небольшой тестовый текст по проверка внешнего вида.',
+    date: 'Апреля 10, 2025',
+    image:
+      'https://images.unsplash.com/photo-1522071820081-009f0129c71c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
+  },
+  {
+    id: 4,
+    title: 'Создание странички',
+    content: 'Полный текст статьи 4...',
+    excerpt: 'Небольшой тестовый текст по проверка внешнего вида.',
+    date: 'Апреля 10, 2025',
+    image:
+      'https://images.unsplash.com/photo-1522071820081-009f0129c71c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
+  },
   // {
   //   id: 5,
   //   title: 'Создание странички',
