7bfaeacd2b7d1415e97d3431be9ee7625a4f263b
Nnder <asfahaikrkt6k56@yandex.ru>
2025-05-07 01:59:28 +0300

    данные для свайпера

--- frontend/nuxt-app/pages/index.vue
+++ frontend/nuxt-app/pages/index.vue
@@ -12,6 +12,33 @@ const { data: category } = await useAsyncData(`category-list`, () => $fetch(`${b
   revalidate: 3600,
 })
 
+// const { data: slides } = await useAsyncData(`slides`, () => $fetch(`${backendUrl}/api/swiper`), {
+//   revalidate: 3600,
+// })
+
+const slidesTime = ref([
+  {
+    brand: 'Зубр',
+    src: 'https://cdn.vseinstrumenti.ru/res/content/page_templates/e37a4e459931bb202ac81144bf28a0a5.jpeg',
+    url: '/news/1',
+  },
+  {
+    brand: 'STIHL',
+    src: 'https://cdn.vseinstrumenti.ru/res/content/page_templates/8cda9503758e95e5abf3820ae1278488.jpeg',
+    url: '/news/2',
+  },
+  {
+    brand: 'Сибур',
+    src: 'https://cdn.vseinstrumenti.ru/res/content/page_templates/5db4e95cb28531ba151e5df026156bcd.jpeg',
+    url: '/news/3',
+  },
+  {
+    brand: 'Haier',
+    src: 'https://cdn.vseinstrumenti.ru/res/content/page_templates/4ce23a87a706eaf19ce2b4d12471a56c.jpeg',
+    url: '/news/4',
+  },
+])
+
 console.log(category.value, 'category')
 </script>
 
@@ -19,7 +46,7 @@ console.log(category.value, 'category')
   <div class="bg-white w-full ~text-sm/xs ~pb-4/8">
     <div class="mx-auto w-full max-w-screen-2xl px-8 space-y-8 py-8">
       <div>
-        <BigSwiper />
+        <BigSwiper :slides="slidesTime" />
       </div>
 
       <div>
