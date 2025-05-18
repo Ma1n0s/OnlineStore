97030ac78eb9215a61381745df8ec6e5707c8190
Ma1n0s <kirill000e@gmail.com>
2025-05-18 14:23:03 +0500

    доб активную менюшку

--- frontend/nuxt-app/components/Account/SidebarMenu.vue
+++ frontend/nuxt-app/components/Account/SidebarMenu.vue
@@ -5,6 +5,7 @@
         <NuxtLink
           to="/account/personal"
           class="flex items-center gap-3 p-2 text-gray-600 hover:bg-gray-50 rounded-lg transition-colors text-sm hover:shadow-sm"
+          active-class="bg-gray-100 text-primary font-medium"
         >
           <Icon name="tabler:user" class="w-6 h-6" />
           Личные данные
@@ -13,6 +14,7 @@
         <NuxtLink
           to="/account/cart-checkout"
           class="flex items-center gap-3 p-2 text-gray-600 hover:bg-gray-50 rounded-lg transition-colors text-sm hover:shadow-sm"
+          active-class="bg-gray-100 text-primary font-medium"
         >
           <Icon name="tabler:basket" class="w-6 h-6" />
           Корзина
@@ -21,6 +23,7 @@
         <NuxtLink
           to="/account/bonus"
           class="flex items-center gap-3 p-2 text-gray-600 hover:bg-gray-50 rounded-lg transition-colors text-sm hover:shadow-sm"
+          active-class="bg-gray-100 text-primary font-medium"
         >
           <Icon name="tabler:gift" class="w-6 h-6" />
           Бонусы
@@ -29,6 +32,7 @@
         <NuxtLink
           to="/account/companies"
           class="flex items-center gap-3 p-2 text-gray-600 hover:bg-gray-50 rounded-lg transition-colors text-sm hover:shadow-sm"
+          active-class="bg-gray-100 text-primary font-medium"
         >
           <Icon name="tabler:home" class="w-6 h-6" />
           Мои организации
@@ -37,6 +41,7 @@
         <NuxtLink
           to="/account/orders"
           class="flex items-center gap-3 p-2 text-gray-600 hover:bg-gray-50 rounded-lg transition-colors text-sm hover:shadow-sm"
+          active-class="bg-gray-100 text-primary font-medium"
         >
           <Icon name="tabler:clipboard-list" class="w-6 h-6" />
           История заказов
@@ -45,6 +50,7 @@
         <NuxtLink
           to="/account/feedback"
           class="flex items-center gap-3 p-2 text-gray-600 hover:bg-gray-50 rounded-lg transition-colors text-sm hover:shadow-sm"
+          active-class="bg-gray-100 text-primary font-medium"
         >
           <Icon name="tabler:message" class="w-6 h-6" />
           Обратная связь
