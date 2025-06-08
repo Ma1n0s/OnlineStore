503cfd26059e66f8202446958218a1086c801b68
Ma1n0s <kirill000e@gmail.com>
2025-06-08 14:51:42 +0500

    +маска для телефона

--- frontend/nuxt-app/pages/account/personal.vue
+++ frontend/nuxt-app/pages/account/personal.vue
@@ -227,6 +227,34 @@ const changePassword = async () => {
   }
 }
 
+const handlePhoneInput = (e, field = 'profile') => {
+  const input = e.target
+  let value = input.value.replace(/\D/g, '')
+  let formattedValue = ''
+
+  if (value.length > 0) {
+    formattedValue = '+7 ('
+    if (value.length > 1) {
+      formattedValue += value.substring(1, 4)
+    }
+    if (value.length >= 4) {
+      formattedValue += ') ' + value.substring(4, 7)
+    }
+    if (value.length >= 7) {
+      formattedValue += '-' + value.substring(7, 9)
+    }
+    if (value.length >= 9) {
+      formattedValue += '-' + value.substring(9, 11)
+    }
+  }
+
+  if (field === 'profile') {
+    profile.value.phone = formattedValue
+  } else {
+    forms.company.phone = formattedValue
+  }
+}
+
 onMounted(() => {
   loadProfile()
 })
@@ -338,9 +366,9 @@ onMounted(() => {
                   <input
                     type="tel"
                     id="phone"
-                    maxlength="18"
-                    placeholder="+7 (___) ___-__-__"
                     v-model="profile.phone"
+                    @input="handlePhoneInput"
+                    placeholder="+7 (___) ___-__-__"
                     class="w-full px-4 py-2 rounded-lg shadow-sm focus:ring-2 focus:ring-primary focus:ring-opacity-50 transition"
                   />
                 </div>
